e6e54f19b7bdbf82fc886df77661f692360f8d08
TimGoeppel <81766887+TimGoeppel@users.noreply.github.com>
2024-12-29 11:01:26 +0100

    Terminplan 2025

--- images/thumbnail.py
+++ images/thumbnail.py
@@ -1,13 +1,20 @@
-import os, sys
+import os, sys, re
 from PIL import Image
 
-for file in os.listdir('.'):
-    print(file)
-    try:
-        image = Image.open(file)
-        s = image.size;
-        size2 = 200, s[1] * 200 / s[0]
-        image.thumbnail(size2, Image.LANCZOS)
-        image.save(os.path.splitext(file)[0]+"-thumb.jpeg", "JPEG");
-    except IOError:
-        print("No thumbnail for %s" % file)
+pattern = re.compile(r"(-thumb\.\w+|thumbnail\.py)$")
+files = os.listdir('.')
+for file in files:
+    if not pattern.search(file):
+        try:
+            image = Image.open(file)
+            new_path = os.path.splitext(file)[0]+"-thumb."
+            if new_path + "jpeg" in files or new_path + "jpg" in files or new_path + "png" in files:
+                print('Thumbnail already generated for ' + file)
+                continue
+            s = image.size;
+            size2 = 200, s[1] * 200 / s[0]
+            image.thumbnail(size2, Image.LANCZOS)
+            image.save(new_path + "jpeg", "JPEG");
+            print(new_path + "jpeg")
+        except IOError:
+            print("No thumbnail for %s" % file)
